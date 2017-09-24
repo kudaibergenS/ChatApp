4a4d278e238e7615eaaea737520e1d1671e2ec25
Sanzhar Kydaybergen <sanzhar.k.96@gmail.com>
2017-09-24 23:28:08 +0600

    Added a Login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from "react-native";
+
+export default class Login extends Component {
+
+    render(){
+
+        return (
+            <View style={styles.container}>                
+                <View style={styles.logoContainer}>                    
+                    <Image 
+                        style={styles.logo}
+                        source={require('../../resources/images/octocat.png')} />
+                        <Text style={styles.title}>An app made for github using React Native</Text>
+                </View> 
+                
+                <View style={styles.formContainer}>                    
+
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#3498db'
+    },
+    logoContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logo: {
+        width: 100,
+        height: 100
+    },
+    title: {
+        color: '#ffffff',
+        marginTop: 10,
+        width: 160,
+        textAlign: 'center',
+        opacity: 0.8
+    }
+});
